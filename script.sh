38c42fef9a4e1d7a1ee7844202645b24f346eb12
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 14:38:20 -0300

    xampp

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget -O xampp-installer.run https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/5.5.28/#:~:text=xampp%2Dlinux%2Dx64%2D5.5.28%2D0%2Dinstaller.run &&
+sudo wget -O xampp-installer.run https://drive.google.com/file/d/1C_OseU-Yr6U7hhOG4Y2I2ZT8Pq1MnD7k/view?usp=sharing &&
 sudo chmod +x xampp-installer.run && sudo ./xampp-installer.run &&
 sudo /opt/lampp/lampp start && sudo apt install net-tools && 
 sudo wget -O sgalivre https://drive.google.com/file/d/1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC/view?usp=sharing &&
