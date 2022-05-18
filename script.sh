4c0de72dbb6fc3f93c435a12e94deb8707f2f6eb
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 14:57:09 -0300

    teste

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget 'https://drive.google.com/file/d/1dwblCrtuMjH1rVe1ATwkIpZXwH7n9TTY/view?usp=sharing' --compressed &&
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc? export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc? export=download&id=1dwblCrtuMjH1rVe1ATwkIpZXwH7n9TTY' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1dwblCrtuMjH1rVe1ATwkIpZXwH7n9TTY" -O xampp-installer.run.zip && rm -rf /tmp/cookies.txt &&
 sudo unzip xampp-installer.run.zip &&
 sudo chmod +x xampp-installer.run && sudo ./xampp-installer.run &&
 sudo /opt/lampp/lampp start && sudo apt install net-tools && 
