7f9833f823bc0a9c462c82a07fc5a7d839dfd341
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 14:48:43 -0300

    unzip

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo wget -O xampp-installer.run https://drive.google.com/file/d/1C_OseU-Yr6U7hhOG4Y2I2ZT8Pq1MnD7k/view?usp=sharing &&
+sudo wget 'https://drive.google.com/file/d/1dwblCrtuMjH1rVe1ATwkIpZXwH7n9TTY/view?usp=sharing' --compressed &&
+sudo unzip xampp-installer.run.zip &&
 sudo chmod +x xampp-installer.run && sudo ./xampp-installer.run &&
 sudo /opt/lampp/lampp start && sudo apt install net-tools && 
-sudo wget -O sgalivre https://drive.google.com/file/d/1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC/view?usp=sharing &&
+sudo wget -O sgalivre 'https://drive.google.com/file/d/1Qy_ciwd1xLoo8hu4eVnz7N5pLdJBUfxk/view?usp=sharing' --compressed &&
+sudo unzip sgalivre.zip &&
 sudo cp -R /opt/lampp/htdocs ./sgalivre
