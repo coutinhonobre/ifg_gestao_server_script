cfe0ec57b1f1854f207cc27e1c6ccbe1a00f8157
Igor Nobre <ferreiranobre@outlook.com>
2022-05-18 15:17:01 -0300

    cp file

--- script.sh
+++ script.sh
@@ -6,4 +6,4 @@ sudo chmod +x xampp-installer.run && sudo ./xampp-installer.run &&
 sudo /opt/lampp/lampp start && sudo apt install net-tools && 
 sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC" -O sgalivre.zip && rm -rf /tmp/cookies.txt &&
 sudo unzip sgalivre.zip &&
-sudo cp -R /opt/lampp/htdocs ./sgalivre
+sudo cp -R sgalivre /opt/lampp/htdocs
